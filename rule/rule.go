eb19b9ba53d0d93ebe629c134cc7dc5587c2b237
Ali Najafizadeh <a.najafizadeh@gmail.com>
2020-03-05 19:32:21 -0500

    Add Rule interface and path repalce

---
+++ rule/rule.go
@@ -0,0 +1,81 @@
+package rule
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alinz/baker.go/internal/errors"
+)
+
+// Handler is an interface to define the logic
+// to ApplyRule to incoming request
+type Handler interface {
+	Valid() error
+	ApplyRule(next http.Handler) http.Handler
+}
+
+// Handlers is a type to implement UnmarshalJSON method
+// every time a new rule is imeplemented, it needs to be added to
+// append method
+type Handlers []Handler
+
+// append converts and valid the rule before adding it to the array
+func (h *Handlers) append(name string, raw json.RawMessage) error {
+	var handler Handler
+
+	switch name {
+	case "path_replace":
+		handler := &PathReplace{}
+		err := json.Unmarshal(raw, handler)
+		if err != nil {
+			return err
+		}
+
+		// valid if loaded rule config is valid
+		err = handler.Valid()
+		if err != nil {
+			return err
+		}
+
+	default:
+		return errors.Value("unknown " + name + " hanlder's name")
+	}
+
+	*h = append(*h, handler)
+	return nil
+}
+
+// Rule
+//
+
+// UnmarshalJSON overrides and decode rule handler configuration based on name
+func (h *Handlers) UnmarshalJSON(p []byte) error {
+	var rawMessages []json.RawMessage
+
+	err := json.Unmarshal(p, &rawMessages)
+	if err != nil {
+		return err
+	}
+
+	// example of how a rule is configured in json payload
+	// { "name": "path_replace", "config": { "search": "/api", "repalce": "", "times": 1 } }
+	//
+	rule := struct {
+		Name   string          `json:"name"`
+		Config json.RawMessage `json:"config"`
+	}{}
+
+	for _, rawMessage := range rawMessages {
+		err = json.Unmarshal(rawMessage, &rule)
+		if err != nil {
+			return err
+		}
+
+		err = h.append(rule.Name, rule.Config)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
