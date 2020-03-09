babebd1038c05c6b52a4b8e31c4547665ceb5dd0
Ali Najafizadeh <a.najafizadeh@gmail.com>
2020-03-09 19:07:35 -0400

    Update base engine

--- engine/engine.go
+++ engine/engine.go
@@ -1,11 +1,13 @@
 package engine
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/alinz/baker.go"
 	"github.com/alinz/baker.go/driver"
@@ -23,12 +25,132 @@ func normalizeHost(host string) (string, bool) {
 }
 
 type Engine struct {
+	client  *http.Client
 	store   *baker.Store
 	watcher driver.Watcher
 	mux     sync.RWMutex
 }
 
 var _ http.Handler = (*Engine)(nil)
+
+func (e *Engine) configs(container *baker.Container) ([]*baker.Config, error) {
+	configURL, err := addr.Join(container.RemoteAddr, container.ConfigPath)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := e.client.Get(configURL)
+	if err != nil {
+		return nil, err
+	}
+
+	var configs []*baker.Config
+
+	err = json.NewDecoder(resp.Body).Decode(&configs)
+	if err != nil {
+		return nil, err
+	}
+
+	return configs, nil
+}
+
+func (e *Engine) Start() error {
+	// watcher
+	watcher := make(chan *baker.Container, 100)
+	go func() {
+		defer close(watcher)
+
+		for {
+			container := e.watcher.Container()
+			if container == nil {
+				return
+			}
+
+			watcher <- container
+		}
+	}()
+
+	// pulser
+	pulser := make(chan *baker.Container, 100)
+	go func() {
+		defer close(pulser)
+
+		deletedContainers := make([]*baker.Container, 0)
+		containersMap := make(map[string]*baker.Container)
+		pulse := time.After(10 * time.Second)
+
+		for {
+			select {
+			case <-pulse:
+				// send deleted items
+				for _, container := range deletedContainers {
+					pulser <- container
+				}
+				// clear deleted array
+				deletedContainers = deletedContainers[:0]
+
+				// loop over all items inside map
+				for _, container := range containersMap {
+					pulser <- container
+				}
+
+				// setup the next tick
+				pulse = time.After(10 * time.Second)
+
+			case container, ok := <-watcher:
+				if !ok {
+					return
+				}
+
+				if container.Active {
+					containersMap[container.ID] = container
+				} else {
+					delete(containersMap, container.ID)
+					deletedContainers = append(deletedContainers, container)
+				}
+			}
+		}
+	}()
+
+	// pinger
+	pinger := make(chan *baker.Target, 100)
+	go func() {
+		defer close(pinger)
+
+		for container := range pulser {
+			configs, err := e.configs(container)
+
+			if err != nil {
+				container.Err = err
+				container.Active = false
+				pinger <- &baker.Target{
+					Container: container,
+					Config:    nil,
+				}
+			} else {
+				for _, config := range configs {
+					pinger <- &baker.Target{
+						Container: container,
+						Config:    config,
+					}
+				}
+			}
+		}
+	}()
+
+	// updater
+	for target := range pinger {
+		e.mux.Lock()
+		if !target.Container.Active || target.Container.Err != nil {
+			e.store.Remove(target.Container)
+		} else {
+			e.store.Add(target.Container, target.Config)
+		}
+		e.mux.Unlock()
+	}
+
+	return nil
+}
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	host := r.Host
@@ -79,5 +201,6 @@ func New(watcher driver.Watcher) *Engine {
 	return &Engine{
 		store:   baker.NewStore(),
 		watcher: watcher,
+		client:  &http.Client{},
 	}
 }
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -1,21 +1,20 @@
 package engine_test
 
 import (
-	"context"
 	"testing"
 
 	"github.com/alinz/baker.go"
 	"github.com/alinz/baker.go/driver"
 	"github.com/alinz/baker.go/engine"
+	"github.com/alinz/baker.go/internal/rule"
 )
 
 type mockedWatcher struct {
-	canel      func()
 	containers chan *baker.Container
 }
 
-func (mw *mockedWatcher) Containers(ctx context.Context) <-chan *baker.Container {
-	return mw.containers
+func (mw *mockedWatcher) Container() *baker.Container {
+	return <-mw.containers
 }
 
 func mockWatcher(containers ...*baker.Container) driver.Watcher {
@@ -34,6 +33,18 @@ func mockWatcher(containers ...*baker.Container) driver.Watcher {
 }
 
 func TestEngine(t *testing.T) {
+	err := rule.Register(&rule.PathReplaceRegistry{})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	engine.New()
+	watcher := mockWatcher()
+
+	proxy := engine.New(watcher)
+
+	err = proxy.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
