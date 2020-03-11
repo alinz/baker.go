51711fd09b34d37fcead7a460ddf9f3e477d8d3d
Ali Najafizadeh <a.najafizadeh@gmail.com>
2020-03-11 03:27:12 -0400

    Add more comments

--- driver/driver.go
+++ driver/driver.go
@@ -4,7 +4,9 @@ import (
 	"github.com/alinz/baker.go"
 )
 
+// Watcher defines how driver should react
 type Watcher interface {
 	// Container gets the next availabel container
+	// this is a blocking calls
 	Container() *baker.Container
 }
